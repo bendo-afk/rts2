9db142684d6e15588dd4e18ccf1f1e2a53b36803
kouyama <233895916+bendo-afk@users.noreply.github.com>
2025-12-27 01:57:52 +0900

    made shoot system, but need to make judge algo if the enemy is visible from the unit

--- scripts/shoot_system.gd
+++ scripts/shoot_system.gd
@@ -3,3 +3,19 @@ extends System
 var units: Array[Unit]
 var map: TileMapLayer
 var teams: Node
+
+var angle_margin := 0.1
+
+func shoot() -> void:
+	for u in units:
+		if u.attack_comp.left_reload_time == 0:
+			var min_angle_diff: float = 4
+			var target_enemy: Unit
+			for v in units:
+				if v.team != u.team and v.vision_comp.visible_state == 2:
+					var angle_diff: float = abs(angle_difference((v.position - u.position).angle(), u.attack_comp.turret_angle))
+					if angle_diff < angle_margin:
+						min_angle_diff = angle_diff
+						target_enemy = v
+			if min_angle_diff <= angle_margin:
+				u.attack_comp.shoot(target_enemy)
