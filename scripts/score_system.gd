9e82a1e9576786c29285b9f546bd3c17e85cb18c
kouyama <233895916+bendo-afk@users.noreply.github.com>
2025-12-28 14:57:51 +0900

    score system first commit

--- scripts/score_system.gd
+++ scripts/score_system.gd
@@ -2,3 +2,17 @@ extends System
 
 var units: Array[Unit]
 var map: TileMapLayer
+var teams: Node
+
+var ally_tiles: Array[Vector2i]
+var enemy_tiles: Array[Vector2i]
+
+
+func _on_tiled_changed(team: Team, tile: Vector2i) -> void:
+	if team == teams.ally:
+		ally_tiles.append(tile)
+	else:
+		enemy_tiles.append(tile)
+
+func get_score() -> void:
+	pass
